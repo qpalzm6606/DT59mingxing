bde177fe315a226828b864b70b18eb03c846faa1
50379 <50379@DESKTOP-JS50U1Q>
2017-12-26 18:06:34 +0800

    ddddddd

---
+++ src/main/java/lesson3/cat.java
@@ -0,0 +1,15 @@
+package lesson3;
+
+public class cat {
+      String name;
+     static String sex;
+       int age;
+      public cat(){
+    	  name = "喵喵";
+      }
+       cat(String name,int age){
+    	  this.name = name;
+    	  this.age = age;
+    	  
+      }
+}
