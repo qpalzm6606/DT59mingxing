4576e204a9d5faf08be5c0652d0d27971fd8eea0
50379 <50379@DESKTOP-JS50U1Q>
2018-01-04 20:23:32 +0800

    Hw20180104

---
+++ src/main/java/Hw20180104/TestString.java
@@ -0,0 +1,69 @@
+/**
+ * Project Name:dt59homework
+ * File Name:TestString.java
+ * Package Name:Hw20180104
+ * Date:2018年1月4日下午8:19:56
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ *
+*/
+
+package Hw20180104;
+
+import java.io.Console;
+
+/**
+ * Description:	   <br/>
+ * Date:     2018年1月4日 下午8:19:56 <br/>
+ * @author   dingP
+ * @version  
+ * @see 	 
+ */
+ /**
+  *  创建字符串的两种方法：
+　　1.直接量：var str = "";
+　　2.字符串对象创建： new String("");
+ 
+String对象的属性
+　　1.constrcutor
+　　2.prototype
+　　3.length:   str.length可以获取字符串长度
+ 
+String对象的方法
+　　1.匹配相关：与正则表达式配合使用
+　　　　（1）str.match(RegExp):匹配指定字符串
+　　　　（2）str.replace(RegExp):替换指定字符
+　　　　（3）str.search(RegExp):查找字符
+ 
+　　2.索引相关：
+　　　　（1）charAt(index):通过索引值查找，返回指定字符
+　　　　（2）indexOf(val):返回指定字符首次出现的位置
+　　　　（3）lastIndexOf(val):返回指定字符最后出现的位置
+　　　　（4）charCodeAt(index):返回指定字符的Unicode编码，介于0 - 65535的数字
+  * 
+  * 
+  * 
+  * 
+  * 
+  * 
+  * 
+  * 
+  * 
+  * 
+  * 
+  * 
+  * 
+  * 
+  * 
+  * Description: <br/>
+  * date: 2018年1月4日 下午8:20:37 <br/>
+  *
+  * @author dingP
+  * @version
+  */
+
+public class TestString {
+   
+    }
+    
+
+
